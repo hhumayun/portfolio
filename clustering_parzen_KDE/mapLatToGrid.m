24a62da6b435c44cebb4647544d7ce106ed0dd8f
Hamza Humayun <hhumayun@Macs-MacBook-Pro.local>
2018-10-10 20:46:58 -0400

    Removed previous Directory

--- clustering_parzen_KDE/mapLatToGrid.m
+++
@@ -1,5 +0,0 @@
-function gridY = mapLatToGrid(y_Size,lat)
-   
-    gridsPerLat  = y_Size/(74.68126 -  73.98826);
-    gridY = floor(gridsPerLat * (lat - 73.98826) );
-end 
--- clustering_parzen_KDE/mapLongToGrid.m
+++
@@ -1,4 +0,0 @@
-function gridX = mapLongToGrid(x_Size,long)
-    gridsPerLong = x_Size/(31.74310 -  31.1850);
-    gridX = floor(gridsPerLong * (long - 31.1850));
-end
